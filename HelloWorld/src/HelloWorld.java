378dac8cbff0599d9a1895087924be92c12fc55f
Justin <jluce89@gmail.com>
2017-06-24 12:48:10 -0400

    updated header comment format

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,9 @@
+/**
+ * File Name   : HelloWorld.java
+ * Author      : Justin Luce
+ * Created on  : 24 June 2017
+ * Description : Displays "Hello World!" to the user
+ **/
 
 public class HelloWorld {
 
