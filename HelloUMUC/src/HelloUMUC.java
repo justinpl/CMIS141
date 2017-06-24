336bc003b0b0fb76a29b81b0be897f999ac0694e
Justin <jluce89@gmail.com>
2017-06-24 15:33:49 -0400

    updated for exercise

--- HelloUMUC/src/HelloUMUC.java
+++ HelloUMUC/src/HelloUMUC.java
@@ -2,14 +2,17 @@
  * File Name   : HelloUMUC.java
  * Author      : Justin Luce
  * Created on  : 24 June 2017
- * Description : Displays "Hello Justin!" to the user
+ * Description : Displays text to the user
  **/
 
 public class HelloUMUC 
 {
 	public static void main(String[] args)
 	{
-		// Display the string.
-		System.out.println("Hello Justin!");
+		// Print lines
+		System.out.println("Welcome to CMIS 141 at UMUC!");
+		System.out.println("Foo");
+		System.out.println("Bar");
+		System.out.println("Baz");
 	}
 }
