741f70bbbf630fd80808d985c7ac9d546186e4f2
Justin <Justin@VAIO.hsd1.md.comcast.net>
2017-06-28 13:10:43 -0400

    Revert "changed "hello World" to "hello Class""
    This reverts commit f0fd687072e16ca0422e6719dab8febdbd23e809.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,14 +2,14 @@
  * File Name   : HelloWorld.java
  * Author      : Justin Luce
  * Created on  : 24 June 2017
- * Description : Displays "Hello Class!" to the user
+ * Description : Displays "Hello World!" to the user
  **/
 
 public class HelloWorld 
 {
 	public static void main(String[] args) 
 	{
-		// Print ""Hello Class!"
-		System.out.println("Hello Class!");
+		// Print ""Hello World!"
+		System.out.println("Hello World!");
 	}
 }
